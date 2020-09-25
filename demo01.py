7bc9c95900f8d2a2e0b4d4239b7e0a6a260bb580
ydl1231232 <1123539310@qq.com>
2020-09-25 11:25:57 +0800

    第一次测试训练

---
+++ demo01.py
@@ -0,0 +1,48 @@
+"""
+print("你好，hello world!") #字符串
+print(2333) #整数
+print(2.333) #小数
+print(True) #布尔值(一般用来判断) True False
+print(()) #元祖
+print([]) #数组
+print({}) #字典
+print("嘻嘻",2333,2.333)
+print("哈哈"+"嘻哈")
+print(((1+2)*100-99)/2)
+print(4>3)
+
+   变量
+   赋值
+
+a = "张三"#把张三这个值赋值给了名字叫a的这个变量#
+print(a)
+"""
+# a = float(input("请输入:"))
+# b = float(input("请输入:"))
+# print("input获取内容:",a+b)
+#数据格式转换
+# print(type("哈哈笑"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+#整数转换成小数
+# a = int(input("请输入整数:"))
+# b =int(input("请输入整数："))
+# print("输出结果：",float(a+b))
+
+#小数转换成整数
+# a = float(input("请输入小数："))
+# b = float(input("请输入小数："))
+# print("输出结果：",int(a+b))
+
+# """
+# 练习：通过代码获取两端内容，并且计算他们的长度和
+# """
+# a = input("请输入：")
+# b = input("请输入：")
+# print("输出结果:",len(a+b),"位")
+
+
